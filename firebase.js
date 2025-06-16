bdec3d413ba85d0a49d33e8e2e683733bd90592a
Dave Clouie Lising <164304088+daveclouie@users.noreply.github.com>
2025-06-16 20:29:27 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration object containing all necessary credentials and settings
+// This configuration is used to initialize Firebase services throughout the application
+export const firebaseConfig = {
+  // API key for Firebase project authentication
+  apiKey: "AIzaSyATAj7J2tYbkwn9X2o6AbmZeHqHX-jE6OY",
+  // Domain for Firebase Authentication
+  authDomain: "lostfound-e304f.firebaseapp.com",
+  // URL for Realtime Database
+  databaseURL: "https://lostfound-e304f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  // Firebase project identifier
+  projectId: "lostfound-e304f",
+  // Storage bucket for Firebase Storage
+  storageBucket: "lostfound-e304f.firebasestorage.app",
+  // Sender ID for Firebase Cloud Messaging
+  messagingSenderId: "963945204977",
+  // Firebase application identifier
+  appId: "1:963945204977:web:bd3b2cd467cb1cdf336d50",
+  // Google Analytics measurement ID
+  measurementId: "G-GWNBNQQQDJ"
+};
